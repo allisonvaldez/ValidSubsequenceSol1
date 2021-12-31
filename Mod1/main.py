7237e1fa9a181b9f2762ffa658e3114958230620
Allison Valdez <ajsnipes@usfca.edu>
2021-12-31 14:46:23 -0800

    finished with notes

--- Mod1/main.py
+++ Mod1/main.py
@@ -1,22 +1,33 @@
 # time: O(n) n represents the length of the array | space: O(1)
 
 
-def test_valid_subsequence(array, sequence):
+def test_valid_subsequence(array, sequent_array):
     """
     begin the code with first initializing the index for both the array and
     subsequence array
     """
-    arrayIndex = 0
-    sequenceIndex = 0
+    array_index = 0
+    sequent_index = 0
 
     """
     create a while loop that traverses both arrays with incrementing the
-    indexes of both arrays when a match is found from each array.
+    index of the subsequent array first when a match is found from the 
+    original array (first array).
     """
-    while arrayIndex < len(array) and sequenceIndex < len(sequence):
-        if array[arrayIndex] == sequence[sequenceIndex]:
-            sequenceIndex += 1
-        arrayIndex += 1
+    while array_index < len(array) and sequent_index < len(sequent_array):
+        if array[array_index] == sequent_array[sequent_index]:
+            sequent_index += 1
+        """
+        regardless if a match is found move forward in the first array 
+        to continue to compare the arrays
+        """
+        array_index += 1
 
-    # return the sequence index at that particular length of the sequence
-    return sequenceIndex == len(sequence)
+    """
+    Returns true if we found all items in the original array that's equal to 
+    the length of the sequent array (all items of the sequent array are found).
+    """
+    return sequent_index == len(sequent_array)
+
+
+print(test_valid_subsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]))
